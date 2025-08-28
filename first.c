11131fb325406e3ef62bab7faf97d0251446eb06
Saiyam Gupta <saiyamg369@gmail.com>
2025-08-28 19:45:38 +0530

    Created a new program to add two numbers

---
+++ first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    printf("This is a sample program to try out how git and github work\n");
+    int a,b,s;
+    printf("Enter first no : ");
+    scanf("%d",&a);
+    printf("Enter second no : ");
+    scanf("%d",&b);
+    s=a+b;
+    printf("The Sum is = %d",s);
+    return 0;
+}
